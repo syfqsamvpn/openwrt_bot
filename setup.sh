3d9556da6ac34c3f5defbd12c03e9368793ee7b8
SYFQSAM <89333348+syfqsamvpn@users.noreply.github.com>
2023-12-19 20:02:16 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+clear
+
+read -p "Bot token : " token
+read -p "Admin Id  : " admin_id
+echo "$token" >/root/databot.txt
+echo "$admin_id" >>/root/databot.txt
+
+opkg install jq --force-depends
+opkg install screen --force-depends
+
+curl -s "https://raw.githubusercontent.com/syfqsamvpn/openwrt_bot/main/openwrt.sh" >/root/bot_tele.sh
+chmod +x /root/bot_tele.sh
+
+screen -dmS bot_sam /root/bot_tele.sh
